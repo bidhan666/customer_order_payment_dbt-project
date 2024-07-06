e13cd8e818ffe84fda0a3e11c25339c8950245d3
Bidhan Pant <s5552673@bournemouth.ac.uk>
2024-07-06 11:42:15 +0000

    developed payments with costumers

--- models/marketing/customers.sql
+++ models/marketing/customers.sql
@@ -1,38 +1,49 @@
-with customers as (
-    select * 
-    from {{ref("jaffle_shop_customers")}}
+WITH customers AS (
+    SELECT * 
+    FROM {{ ref("jaffle_shop_customers") }}
 ),
 
-orders as (
-    select *
-    from {{ref("jaffle_shop_orders")}}
+orders AS (
+    SELECT *
+    FROM {{ ref('jaffle_shop_orders') }}
 ),
 
-customer_orders as (
-    select
-        user_id,
-        min(order_date) as first_order,
-        max(order_date) as most_recent_order,
-        count(id) as number_of_orders
-
-    from orders
-    group by user_id
+stripe_payments AS (
+    SELECT *
+    FROM {{ ref('stripe_payments') }}
 ),
 
+customer_orders AS (
+    SELECT
+        orders.user_id,
+        MIN(orders.order_date) AS first_order,
+        MAX(orders.order_date) AS most_recent_order,
+        COUNT(orders.id) AS number_of_orders,
+        SUM(stripe_payments.amount) AS total_amount_paid,
+        COUNT(stripe_payments.payment_id) AS number_of_payments,
+        MIN(stripe_payments.created_at) AS first_payment,
+        MAX(stripe_payments.created_at) AS most_recent_payment,
+        STRING_AGG(DISTINCT stripe_payments.payment_method, ', ') AS payment_methods
+    FROM orders
+    LEFT JOIN stripe_payments ON orders.id = stripe_payments.order_id
+    GROUP BY orders.user_id
+),
 
-final as (
-    select
+final AS (
+    SELECT
         customers.first_name,
         customers.last_name,
         customer_orders.first_order,
         customer_orders.most_recent_order,
-        customer_orders.number_of_orders
-    
-    from customers
-    left join customer_orders
-    on customers.id = customer_orders.user_id
+        customer_orders.number_of_orders,
+        customer_orders.total_amount_paid,
+        customer_orders.number_of_payments,
+        customer_orders.first_payment,
+        customer_orders.most_recent_payment,
+        customer_orders.payment_methods
+    FROM customers
+    LEFT JOIN customer_orders ON customers.id = customer_orders.user_id
 )
 
-
-select * from final
-
+SELECT * 
+FROM final
